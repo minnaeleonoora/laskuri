99acadc847addbe56c684b501c2a869cf43f430e
Kilpijärvi Minna <s1900557@edu.bc.fi>
2019-09-10 14:39:29 +0300

    buttonit ja väri vaihtelu lisätty

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "./Counter.css";
 
-const Counter = () => {
-    return (
-        <div className="counter">
-            <p>0</p>
-        </div>
-    )
+
+class Counter extends Component {
+
+    state = {
+        counter: 0
+
+    }
+
+    addHandler = () => {
+        this.setState( {
+            counter:this.state.counter +1
+        })
+    }
+    removeHandler = () => {
+        if (this.state.counter > 0) {
+        this.setState( {
+            counter:this.state.counter -1
+        })  
+    }
+    }
+    resetHandler = () => {
+        this.setState( {
+            counter: 0
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className={this.state.counter % 2 === 0 ? "circle even":"circle odd"}>
+                    <p className="number">{this.state.counter}</p>
+                </div>
+                <div className="buttons">
+                    <button onClick= {this.addHandler}>ADD ONE</button>
+                    <button onClick= {this.removeHandler}>REMOVE ONE</button>
+                    <button onClick= {this.resetHandler}>RESET</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Counter;
